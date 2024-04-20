24981d3f42b9308ef215ca0580abae5fe308a9b5
MrTorfick <marcososalulu@hotmail.com>
2024-04-19 23:04:11 -0300

    creacion de ABBs, para diferentes pruebas

---
+++ src/main/java/dominio/ABBGenerics3.java
@@ -0,0 +1,69 @@
+package dominio;
+
+import java.util.Comparator;
+
+public class ABBGenerics3<T> {
+
+    private class NodoABBGeneric {
+        private T dato;
+        private NodoABBGeneric izq;
+        private NodoABBGeneric der;
+
+
+        public NodoABBGeneric(T dato) {
+            this.dato = dato;
+        }
+
+    }
+
+    private NodoABBGeneric raiz;
+    private final Comparator<T> comparador;
+
+    public ABBGenerics3(Comparator<T> comparador) {
+        this.comparador = comparador;
+    }
+
+    public void agregar(T datoAgregar) {
+        this.raiz = agregar(raiz, datoAgregar);
+    }
+
+    public boolean existe(T cascaron) {
+        return existe(raiz, cascaron);
+    }
+
+    public boolean existe(NodoABBGeneric nodo, T datoBuscar) {
+        if (nodo == null) return false;
+        else if (datoBuscar.equals(nodo.dato)) return true;
+        else if (comparador.compare(datoBuscar, nodo.dato) > 0) return existe(nodo.der, datoBuscar);
+        else return existe(nodo.izq, datoBuscar);
+    }
+
+    public void imprimirOrdenado() {
+        imprimirOrdenado(raiz);
+    }
+
+    private void imprimirOrdenado(NodoABBGeneric nodo) {
+        if (nodo == null) return;
+        imprimirOrdenado(nodo.izq);
+        System.out.print(nodo.dato);
+        System.out.print(",");
+        imprimirOrdenado(nodo.der);
+    }
+
+    private NodoABBGeneric agregar(NodoABBGeneric nodo, T datoAgregar) {
+        if (nodo == null) {
+            return new NodoABBGeneric(datoAgregar);
+
+        } else if (nodo.dato.equals(datoAgregar)) throw new RuntimeException("Duplicado");
+
+        else if (comparador.compare(datoAgregar, nodo.dato) > 0) {
+            nodo.der = agregar(nodo.der, datoAgregar);
+            return nodo;
+        } else {
+            nodo.izq = agregar(nodo.izq, datoAgregar);
+            return nodo;
+        }
+    }
+
+
+}
---
+++ src/main/java/dominio/ObjetoComparable.java
@@ -0,0 +1,16 @@
+package dominio;
+
+public interface ObjetoComparable<T> extends Comparable<T> {
+
+    boolean esMayor(T dato);
+
+    boolean esMenor(T dato);
+
+
+    @Override
+    default int compareTo(T o) {
+        if (esMayor(o)) return 1;
+        else if (esMenor(o)) return -1;
+        else return 0;
+    }
+}
